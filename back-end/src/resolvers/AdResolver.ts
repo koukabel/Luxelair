e5fa3439a0e2be9862e47f5bc723e6d54d276bc0
vincentr-wcs <vincent.ravanel@wildcodeschool.com>
2024-03-15 09:57:02 +0100

    changement

--- back-end/src/resolvers/AdResolver.ts
+++ back-end/src/resolvers/AdResolver.ts
@@ -51,8 +51,8 @@ export class AdResolver {
   }
 
   @Query(() => [Ad])
-  search(@Arg("title") title: string) {
-    return Ad.searchAd(title);
+  search(@Arg("location") location: string) {
+    return Ad.searchAd(location);
   }
 
   @Mutation(() => Ad)
