b4082b1dda83dadf06ca82474151af95c24b89e3
Khouloud Belkhir <74536416+koukabel@users.noreply.github.com>
2024-07-25 16:02:00 +0000

    added toasts to handle errors and edited the createAd file

--- web-app/src/components/AdCreationComponents/FirstStep/ControlButtons.tsx
+++ web-app/src/components/AdCreationComponents/FirstStep/ControlButtons.tsx
@@ -1,21 +1,26 @@
 import { Button, Flex, Progress } from "@chakra-ui/react";
-import { useState } from "react";
+
 
 
 interface ControlButtonsProps {
   handleNext: () => void;
   handlePrevious: () => void;
   progressValue: number;
+  hidePreviousButton: boolean;
+  hideNextButton: boolean
+  handleSubmit: () => void;
+  isPublicationButtonShown: boolean
 }
 
 
-const ControlButtons: React.FC<ControlButtonsProps> = ({ handleNext, handlePrevious, progressValue }) => {
+const ControlButtons: React.FC<ControlButtonsProps> = ({ handleNext, handlePrevious, progressValue, hidePreviousButton, hideNextButton, handleSubmit, isPublicationButtonShown  }) => {
   return (
     <div>
       <Progress colorScheme="yellow" size="sm" value={progressValue} />
       <Flex justifyContent="space-between" p={10}>
-        <Button onClick={handlePrevious} bg="black" color="white">Retour</Button>
-        <Button onClick={handleNext} bg="black" color="white">Suivant</Button>
+      {!hidePreviousButton && <Button bg="black" color="white" onClick={handlePrevious}>Retour</Button>}
+      {!hideNextButton && <Button bg="black" color="white" onClick={handleNext}>Suivant</Button>}
+      {isPublicationButtonShown && <Button bg="black" color="white"  onClick={handleSubmit}>Publier mon annonce</Button>}
       </Flex>
     </div>
   );
