8fa8788617c9f3a8069ab23cdc9257618b91d60d
Vincent <vravanel@gmail.com>
2024-07-26 10:32:22 +0200

    ajout test front payment

--- back-end/src/tests/userSession.spec.ts
+++ back-end/src/tests/userSession.spec.ts
@@ -1,37 +1,37 @@
-// // import { getDataSource } from "../database";
-// // import UserSession from "../entities/userSession";
-// // import User from "../entities/user";
+import { getDataSource } from "../database";
+import UserSession from "../entities/userSession";
+import User from "../entities/user";
 
-// // describe("UserSession", () => {
-// //   beforeEach(async () => {
-// //     const database = await getDataSource();
-// //     for (const entity of database.entityMetadatas) {
-// //       const repository = database.getRepository(entity.name);
-// //       await repository.query(
-// //         `TRUNCATE "${entity.tableName}" RESTART IDENTITY CASCADE;`
-// //       );
-// //     }
-// //   });
+describe("UserSession", () => {
+  beforeEach(async () => {
+    const database = await getDataSource();
+    for (const entity of database.entityMetadatas) {
+      const repository = database.getRepository(entity.name);
+      await repository.query(
+        `TRUNCATE "${entity.tableName}" RESTART IDENTITY CASCADE;`
+      );
+    }
+  });
 
-// //   afterAll(async () => {
-// //     const database = await getDataSource();
-// //     await database.destroy();
-// //   });
+  afterAll(async () => {
+    const database = await getDataSource();
+    await database.destroy();
+  });
 
-//   describe("saveNewSession", () => {
-//     it("returns user with session", async () => {
-//       const user = await User.createNewUser({
-//         email: "test@email.com",
-//         firstName: "Test",
-//         lastName: "User",
-//         password: "testpassword",
-//         roles: ["Traveller"],
-//       });
+  describe("saveNewSession", () => {
+    it("returns user with session", async () => {
+      const user = await User.createNewUser({
+        email: "test@email.com",
+        firstName: "Test",
+        lastName: "User",
+        password: "testpassword",
+        roles: ["Traveller"],
+      });
 
-// //       const session1 = await UserSession.saveNewSession(user);
-// //       const session2 = await UserSession.getSessionById(session1.id);
+      const session1 = await UserSession.saveNewSession(user);
+      const session2 = await UserSession.getSessionById(session1.id);
 
-// //       expect(session1.id).toEqual(session2.id);
-// //     });
-// //   });
-// // });
+      expect(session1.id).toEqual(session2.id);
+    });
+  });
+});
